526f5955d5150a9d26dcc6e0dd266aa2b536e338
Yuto Hara <mail@hxrxchang.dev>
2021-12-29 09:11:16 +0000

    embed

---
+++ ch4/struct/embed/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Point struct {X, Y int}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+func main() {
+	w := Wheel{Circle{Point{8, 8}, 5}, 20}
+	fmt.Println(w)
+	w.X = 9
+	fmt.Println(w)
+}
